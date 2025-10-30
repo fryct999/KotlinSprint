9c906eba44db975e20e8f56e420c8048adbdb691
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-31 02:49:36 +0300

    KS-2-2 create payroll calculation

--- src/main/kotlin/lesson_2/lesson_2_2.kt
+++ src/main/kotlin/lesson_2/lesson_2_2.kt
@@ -1,5 +1,19 @@
 package org.example.lesson_2
 
 fun main() {
+    val countWorkers = 50
+    val countStudents = 30
 
+    val payrollWorker = 30000
+    val payrollStudent = 20000
+
+    val payrollAllWorkers = countWorkers * payrollWorker
+    val payrollAllStudents = countStudents * payrollStudent
+    val allPayroll = payrollAllWorkers + payrollAllStudents
+
+    val averagePayroll = (allPayroll / (countWorkers + countStudents)).toInt()
+
+    println(payrollAllWorkers)
+    println(allPayroll)
+    println(averagePayroll)
 }
