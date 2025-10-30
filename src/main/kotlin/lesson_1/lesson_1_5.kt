54ae011c4a213110fb18d9b7157dfc5f1cbf0a15
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-30 05:40:52 +0300

    KS-1-5 task completed

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_1
 
 fun main() {
+    val seconds = 6480
+    val secondsLeft = formatString(seconds % 60)
+    val minute = seconds / 60
+    val minuteLeft = formatString(minute % 60)
+    val hour = formatString(minute / 60)
 
+    println("$hour:$minuteLeft:$secondsLeft")
+}
+
+fun formatString(value: Int): String {
+    return if (value <= 9) "0" + (value).toString() else value.toString()
 }
