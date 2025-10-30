5d3d888f8a005533229ddf4db91a59f5313abe5b
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-30 04:41:03 +0300

    KS-1-2 task completed

--- src/main/kotlin/lesson_1/lesson_1_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_2.kt
@@ -3,6 +3,12 @@ package org.example.lesson_1
 fun main() {
     val numOfOrders: Int = 75
     val purchaseMessage: String = "Thank you for your purchase in our store!"
+    println(numOfOrders)
+    println(purchaseMessage)
 
+    var numOfStaff: Int = 2000
+    //println(numOfStaff)
 
+    numOfStaff -= 1
+    println(numOfStaff)
 }
