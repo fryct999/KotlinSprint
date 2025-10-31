5a9d9ceb57c36302761bfcff166da42f43c62a90
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-31 04:24:39 +0300

    KS-2-5 created percent calc

--- src/main/kotlin/lesson_2/lesson_2_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -1,2 +1,15 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
+fun main() {
+    val deposit = 70000
+
+    val depositLifeTime = 20
+    val yearPercent = 0.167
+    val payPercentInYear = 1
+
+    val totalSum = deposit * (1 + yearPercent / payPercentInYear).pow(payPercentInYear * depositLifeTime)
+
+    println(String.format("%.3f", totalSum))
+}
