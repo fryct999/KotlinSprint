2fe801a5452794a70d64cee2c3ca2eb49a471c3f
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-31 03:30:45 +0300

    KS-2-3 created train schedule function

--- src/main/kotlin/lesson_2/lesson_2_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_3.kt
@@ -1,2 +1,29 @@
 package org.example.lesson_2
 
+const val MINUTES_PER_HOUR = 60
+const val HOUR_PER_DAY = 24
+
+fun main() {
+    val wayTime = 457
+    val startHour = 9
+    val startMinute = 39
+
+    println(calculatingArrivalTime(startHour, startMinute, wayTime))
+}
+
+fun calculatingArrivalTime(startHour: Int, startMinute: Int, wayTime: Int ) : String {
+    var hoursWay = wayTime / MINUTES_PER_HOUR
+    val minutesWayLeft = wayTime % MINUTES_PER_HOUR
+
+    var arrivalMinute = startMinute + minutesWayLeft
+
+    if (arrivalMinute >= 60) {
+        arrivalMinute -= 60
+        hoursWay += 1
+    }
+
+    var arrivalHour = startHour + hoursWay
+    arrivalHour = if (arrivalHour >= HOUR_PER_DAY) arrivalHour - HOUR_PER_DAY else arrivalHour
+
+    return String.format("%02d", arrivalHour) + ":" + String.format("%02d", arrivalMinute)
+}
