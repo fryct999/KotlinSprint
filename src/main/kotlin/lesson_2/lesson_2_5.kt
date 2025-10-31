c4ae0b058cc50c29c58f78699119f8d402167be6
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-01 02:38:30 +0300

    KS-2-5 add const fixed calc, percent and print

--- src/main/kotlin/lesson_2/lesson_2_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -2,14 +2,15 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val MAX_PERCENT = 100
+
 fun main() {
     val deposit = 70000
-
     val depositLifeTime = 20
-    val yearPercent = 0.167
-    val payPercentInYear = 1
+    val yearPercent = 16.7
+    val yearPercentDec = yearPercent / MAX_PERCENT
 
-    val totalSum = deposit * (1 + yearPercent / payPercentInYear).pow(payPercentInYear * depositLifeTime)
+    val totalSum = deposit * (1 + yearPercentDec ).pow(depositLifeTime)
 
-    println(String.format("%.3f", totalSum))
+    println("%.3f".format(totalSum))
 }
