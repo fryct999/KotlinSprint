6051c8c6bcc4db8c035f10d91af63ccc3c97279a
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-31 16:57:48 +0300

    KS-2-1 fixed average

--- src/main/kotlin/lesson_2/lesson_2_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -2,8 +2,7 @@ package org.example.lesson_2
 
 fun main() {
     val grades = arrayOf(3, 4, 3, 5)
-    val average = grades.sum().toFloat() / grades.size.toFloat()
-    val averagePrint = String.format("%.2f", average)
+    val average = grades.sum().toFloat() / grades.size
 
-    println(averagePrint)
+    println("%.2f".format(average))
 }
