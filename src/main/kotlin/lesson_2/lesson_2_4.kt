d17728b4017e7a3c6abb0a3fbb2467758ab4b13a
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-10-31 04:01:21 +0300

    KS-2-4 created buff mining function

--- src/main/kotlin/lesson_2/lesson_2_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_4.kt
@@ -1,2 +1,22 @@
 package org.example.lesson_2
 
+fun main() {
+    val buffEnable = true
+    val buffMining = 0.2f
+
+    val baseCrystalMining = 7
+    val baseIronMining = 11
+
+    val buffCrystalMiningValue = baseCrystalMining * buffMining
+    val buffIronMiningValue = baseIronMining * buffMining
+    val buffCrystalMining = baseCrystalMining + buffCrystalMiningValue
+    val buffIronMining = baseIronMining + buffIronMiningValue
+
+    val factCrystalMining = if (buffEnable) buffCrystalMining else baseCrystalMining
+    val factIronMining = if (buffEnable) buffIronMining else baseIronMining
+
+    println("Current crystal mining - $factCrystalMining")
+    println("Current iron mining - $factIronMining")
+    println("Crystal buff strength - ${buffCrystalMiningValue.toInt()}")
+    println("Iron buff strength - ${buffIronMiningValue.toInt()}")
+}
