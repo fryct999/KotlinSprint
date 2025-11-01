e58bc9766d2c5f93dff5c00f583782d829e72809
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-02 00:30:39 +0300

    KS-4-2 created check average cargo

--- src/main/kotlin/lesson_4/lesson_4_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_2.kt
@@ -1,5 +1,17 @@
 package org.example.lesson_4
 
 fun main() {
+    val minWeight = 35
+    val maxWeight = 100
+    val maxVolume = 100
+    val printMsg = "Cargo with weight %d and the volume of %d liters corresponds to the category \'Average\':%b"
 
+    var cargoWeight = 20
+    var cargoVolume = 80
+    println(String.format(printMsg, cargoWeight, cargoVolume, cargoWeight > minWeight && cargoWeight <= maxWeight && cargoVolume < maxVolume))
+
+    cargoWeight = 50
+    cargoVolume = 100
+    println(String.format(printMsg, cargoWeight, cargoVolume, cargoWeight > minWeight && cargoWeight <= maxWeight && cargoVolume < maxVolume))
 }
+
