9889f88b75ca9ee8519fbd28baba8fca29274bef
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-02 00:45:04 +0300

    KS-4-3 created checking conditions for agrocomplex

--- src/main/kotlin/lesson_4/lesson_4_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_3.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_4
 
-fun main() {
+const val REQUIRED_HUMIDITY = 20
+const val UNFAVORABLE_SEASON = "Winter"
 
+fun main() {
+    val weatherIsSunny = true
+    val tentIsOpen = true
+    val airHumidity = 20
+    val season = "Winter"
+
+    val printMsg = "Are the conditions for growing legumes favorable now? %b"
+
+    println(printMsg.format(weatherIsSunny && tentIsOpen && airHumidity == REQUIRED_HUMIDITY && season != UNFAVORABLE_SEASON))
 }
