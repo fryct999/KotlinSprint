dc3834f28adf0f15464b2f8f1991f2ea6faed75d
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-01 03:31:34 +0300

    KS-3-3 created multiplication table

--- src/main/kotlin/lesson_3/lesson_3_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_3.kt
@@ -1,5 +1,22 @@
 package org.example.lesson_3
 
 fun main() {
+    val num = 3
+    val multiplicationTableForNum = """
+        |${getMultiplicationStr(num, 1)}
+        |${getMultiplicationStr(num, 2)}
+        |${getMultiplicationStr(num, 3)}
+        |${getMultiplicationStr(num, 4)}
+        |${getMultiplicationStr(num, 5)}
+        |${getMultiplicationStr(num, 6)}
+        |${getMultiplicationStr(num, 7)}
+        |${getMultiplicationStr(num, 8)}
+        |${getMultiplicationStr(num, 9)}
+    """.trimMargin()
 
+    println(multiplicationTableForNum)
+}
+
+fun getMultiplicationStr(num : Int, multiplier : Int) : String {
+    return "$num x $multiplier = ${num * multiplier}"
 }
