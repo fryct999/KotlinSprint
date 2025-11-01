9dd4c5d912276c16548e1100edeb59c67ce80b57
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-01 03:16:20 +0300

    KS-3-2 created get personal data

--- src/main/kotlin/lesson_3/lesson_3_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_2.kt
@@ -1,5 +1,16 @@
 package org.example.lesson_3
 
 fun main() {
+    val name = "Tatyana"
+    val oldLastName = "Andreeva"
+    val patronymic = "Sergeevna"
 
+    val newLastName = "Sidorova"
+    val ageOfChangeLastName = 22
+
+    var ageOfRequest = 20
+    println("${if (ageOfRequest < ageOfChangeLastName) oldLastName else newLastName} $name $patronymic, $ageOfRequest")
+
+    ageOfRequest = ageOfChangeLastName
+    println("${if (ageOfRequest < ageOfChangeLastName) oldLastName else newLastName} $name $patronymic, $ageOfRequest")
 }
