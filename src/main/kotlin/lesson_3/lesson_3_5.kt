7f3049d89eb6d4c1e670631c96b8f3687c80a064
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-01 03:53:33 +0300

    KS-3-5 created string parser

--- src/main/kotlin/lesson_3/lesson_3_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_5.kt
@@ -1,5 +1,14 @@
 package org.example.lesson_3
 
 fun main() {
+    val historyString = "D2-D4;0"
 
+    val splitString = historyString.split("[-;]".toRegex())
+    val from = splitString[0]
+    val where = splitString[1]
+    val numStep = splitString[2]
+
+    println(from)
+    println(where)
+    println(numStep)
 }
