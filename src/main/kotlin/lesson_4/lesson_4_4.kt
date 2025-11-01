b9d919d1d1653f92c8e4e14d0226b7872f00c5e6
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-02 01:14:00 +0300

    KS-4-4 created workout calendar

--- src/main/kotlin/lesson_4/lesson_4_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_4.kt
@@ -1,5 +1,23 @@
 package org.example.lesson_4
 
 fun main() {
+    var dayNum = 1
+    println(getTodayExercises(dayNum) + "\n")
 
+    dayNum += 1
+    println(getTodayExercises(dayNum))
+}
+
+fun getTodayExercises(dayNum : Int) : String {
+    val armsAndAbsTraining = dayNum % 2 != 0
+    val legsAndBackTraining = dayNum % 2 == 0
+
+    val workOutMsg = """
+        |Exercises for the arms: $armsAndAbsTraining
+        |Exercises for the legs: $legsAndBackTraining
+        |Exercises for the back: $legsAndBackTraining
+        |Exercises for the abs: $armsAndAbsTraining
+    """.trimMargin()
+
+    return workOutMsg
 }
