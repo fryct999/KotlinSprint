c5616765c3e9d8fec1afe99fbbc14c1cbf2873f4
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 01:52:56 +0300

    KS-5-4 created ship verification

--- src/main/kotlin/lesson_5/lesson_5_4.kt
+++ src/main/kotlin/lesson_5/lesson_5_4.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_5
 
 fun main() {
+    val userNameList = listOf("Zaphod",)
+    val userPasswordMap = mapOf("Zaphod" to "PanGalactic",)
 
+    println("Enter your username:")
+    val userName = readLine()
+
+    println("Enter your password:")
+    val password = readLine()
+
+    if (userNameList.any { it == userName}) {
+        if (userPasswordMap[userName] == password) println("The data is correct! Welcome aboard $userName!")
+        else println("You entered the wrong password!")
+    } else {
+        println("The user with login $userName has not been found! Do you want to register?")
+        return
+    }
 }
