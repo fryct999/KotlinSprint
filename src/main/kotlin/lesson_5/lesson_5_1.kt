aff8ec78f446d6446d979d0ca984994add3cdcb2
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 00:52:24 +0300

    KS-5-1 created сaptcha

--- src/main/kotlin/lesson_5/lesson_5_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_1.kt
@@ -1,5 +1,18 @@
 package org.example.lesson_5
 
-fun main() {
+const val MIN_RANGE_NUM = 1
+const val MAX_RANGE_NUM = 100
 
+fun main() {
+    val range = MIN_RANGE_NUM..MAX_RANGE_NUM
+    val firstNum = range.random()
+    val secondNum = range.random()
+    val sumNum = firstNum + secondNum
+
+    println("Enter the example solution: $firstNum + $secondNum = ?")
+
+    val userAnswer: Int? = readlnOrNull()?.toIntOrNull()
+
+    if (userAnswer == sumNum) println("Access accept!")
+    else println("Access denied!")
 }
