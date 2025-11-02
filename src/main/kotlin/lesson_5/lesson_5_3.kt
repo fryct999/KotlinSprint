25ce9c5fe359091f1f3b519f86b990d924170b67
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 01:24:25 +0300

    KS-5-3 created lottery nums verificator

--- src/main/kotlin/lesson_5/lesson_5_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_3.kt
@@ -1,5 +1,27 @@
 package org.example.lesson_5
 
 fun main() {
+    val firstWinNum = 24
+    val secondWinNum = 37
+    val winNums = listOf(firstWinNum, secondWinNum)
 
+    println("Enter the first number:")
+    val firstNum: Int? = readlnOrNull()?.toIntOrNull()
+
+    println("Enter the second number:")
+    val secondNum: Int? = readlnOrNull()?.toIntOrNull()
+
+    if (firstNum == null || secondNum == null) {
+        println("Incorrect data has been entered!")
+        return
+    }
+
+    val resultMsg = when {
+        firstNum in winNums && secondNum in winNums -> "Congratulations! You have won the grand prize!"
+        firstNum in winNums || secondNum in winNums -> "You've won a consolation prize!"
+        else -> "Fail!"
+    }
+
+    println(resultMsg)
+    println("Winning numbers: $firstWinNum and $secondWinNum")
 }
