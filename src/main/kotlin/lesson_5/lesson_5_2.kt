d0e67bb6f716f4d7bca09362a78125a4e675c1e7
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 01:04:50 +0300

    KS-5-2 created age verification

--- src/main/kotlin/lesson_5/lesson_5_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_2.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_5
 
-fun main() {
+const val AGE_MAJORITY = 18
 
+fun main() {
+    println("Enter the year of birth:")
+    val birthYear: Int? = readLine()?.toIntOrNull()
+
+    if (birthYear == null || birthYear < 0) {
+        println("Incorrect data has been entered!")
+        return
+    }
+
+    val userAge = java.time.Year.now().value - birthYear
+
+    val resultText = if(userAge >= AGE_MAJORITY) "Show a screen with hidden content."
+    else "Back to the main screen"
+
+    println(resultText)
 }
