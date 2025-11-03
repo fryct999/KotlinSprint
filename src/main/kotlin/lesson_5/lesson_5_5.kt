c9cf328d1e2cc0b8bd74558e295fdc136d3e8e04
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 03:04:15 +0300

    KS-5-5 created new lottery function

--- src/main/kotlin/lesson_5/lesson_5_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_5.kt
@@ -1,5 +1,40 @@
 package org.example.lesson_5
 
-fun main() {
+const val MIN_RANGE_NUM = 0
+const val MAX_RANGE_NUM = 42
+const val JACKPOTS_NUM = 3
+const val BIG_PRIZE_NUM = 2
+const val CONSOLATION_PRIZE_NUM = 1
 
+fun main() {
+    val range = MIN_RANGE_NUM .. MAX_RANGE_NUM
+    val winNumsList: List<Int> = listOf(range.random(), range.random(), range.random())
+    val userNumsList = mutableListOf<Int>()
+
+    println("Enter the first number:")
+    val firstNum: Int? = readlnOrNull()?.toIntOrNull()
+
+    println("Enter the second number:")
+    val secondNum: Int? = readlnOrNull()?.toIntOrNull()
+
+    println("Enter the third number:")
+    val thirdNum: Int? = readlnOrNull()?.toIntOrNull()
+
+    if (firstNum == null || secondNum == null || thirdNum == null) {
+        println("Incorrect data has been entered!")
+        return
+    }
+
+    userNumsList.addAll(listOf(firstNum, secondNum, thirdNum))
+
+    val correctAnswer = userNumsList.intersect(winNumsList).size
+    val resultMsg = when(correctAnswer) {
+        JACKPOTS_NUM -> "You guessed all the numbers and won the jackpot!"
+        BIG_PRIZE_NUM -> "You guessed two numbers and gets a big prize!"
+        CONSOLATION_PRIZE_NUM -> "You have guessed one number, and you will receive a consolation prize!"
+        else -> "You haven't guessed a single number!"
+    }
+
+    println(resultMsg)
+    println("Winning numbers: $winNumsList")
 }
