2b4b81614c6d1b7b1c36bd86b12eaaac4c837632
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-06 08:30:12 +0300

    KS-5-8 add elvis and fixed imt category

--- src/main/kotlin/lesson_5/lesson_5_6.kt
+++ src/main/kotlin/lesson_5/lesson_5_6.kt
@@ -4,29 +4,28 @@ import kotlin.math.pow
 
 const val CM_IN_M = 100
 const val HEIGHT_EXPONENT = 2
-const val ZERO_IMT = 0.0
 const val INSUFFICIENT_WEIGHT = 18.5
 const val NORMAL_WEIGHT = 25.0
 const val EXCESS_WEIGHT = 30.0
 
 fun main() {
     println("Enter your weight in kg:")
-    val weight = readLine()?.toDoubleOrNull()
+    val weight = readLine()?.toDouble() ?: 0.0
 
     println("Enter your height in cm:")
-    var height = readLine()?.toDoubleOrNull()
+    var height = readLine()?.toDouble() ?: 0.0
 
-    if (weight == null || height == null || height == 0.0) {
+    if (weight <= 0.0 || height <= 0.0) {
         println("Incorrect data has been entered!")
         return
     }
 
     height /= CM_IN_M
     val imt = weight / height.pow(HEIGHT_EXPONENT)
-    val imtTypeStr = when(imt) {
-        in ZERO_IMT..< INSUFFICIENT_WEIGHT -> "insufficient body weight"
-        in INSUFFICIENT_WEIGHT..< NORMAL_WEIGHT -> "normal body weight"
-        in NORMAL_WEIGHT..< EXCESS_WEIGHT -> "excess body weight"
+    val imtTypeStr = when {
+        imt < INSUFFICIENT_WEIGHT -> "insufficient body weight"
+        imt in INSUFFICIENT_WEIGHT..< NORMAL_WEIGHT -> "normal body weight"
+        imt in NORMAL_WEIGHT..< EXCESS_WEIGHT -> "excess body weight"
         else -> "fatness"
     }
 
