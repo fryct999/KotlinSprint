89b38fc6901d2b8c387dab42460ddd7786b7a735
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-15 12:53:01 +0300

    created password generator

--- src/main/kotlin/lesson_7/lesson_7_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_1.kt
@@ -1,5 +1,15 @@
 package org.example.lesson_7
 
 fun main() {
-    
+    val numRange = 1 .. 9
+    val charRange = 'a' .. 'z'
+    val passwordRange = 1.. 6
+    var password = ""
+
+    for (i in passwordRange ) {
+        if(i % 2 == 0) password += numRange.random()
+        else password += charRange.random()
+    }
+
+    println(password)
 }
