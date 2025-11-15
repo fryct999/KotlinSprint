5d9d8eaffab0b51e6070ea6e03e58a1935920d82
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-15 15:31:46 +0300

    KS-8-2 created ingredient checker

--- src/main/kotlin/lesson_8/lesson_8_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_2.kt
@@ -1,2 +1,17 @@
 package org.example.lesson_8
 
+fun main() {
+    val arrayOfIngredients = arrayOf("Egg", "Tomato", "Green", "Salt", "Pepper")
+
+    println("Enter the name of the ingredient:")
+    val userIngredient = readln()
+
+    for (i in arrayOfIngredients) {
+        if (userIngredient == i) {
+            println("The ingredient $userIngredient is in the recipe.")
+            return
+        }
+    }
+
+    println("There's no such ingredient in the recipe.")
+}
