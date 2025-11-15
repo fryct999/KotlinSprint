045629be5e862f943222f085cdbe7219ca2f8cd0
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-15 13:53:37 +0300

    KS-7-3 add even nums counter

--- src/main/kotlin/lesson_7/lesson_7_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_3.kt
@@ -1,5 +1,18 @@
 package org.example.lesson_7
 
 fun main() {
+    println("Enter number:")
+    val userNum = readln().toInt()
 
+    val lastRangeNum = if(userNum % 2 == 0) userNum else userNum - 1
+
+    if (userNum <= 0) {
+        println("Incorrect data.")
+        return
+    }
+
+    val numRange = 0 .. lastRangeNum step 2
+
+    val msg = "All even numbers in the range: ${numRange.joinToString()}"
+    println(msg)
 }
