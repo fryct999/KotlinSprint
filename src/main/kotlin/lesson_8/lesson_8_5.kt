fee41e0b6811bb31e8da45eaea4943f6eb98dc51
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-15 16:03:26 +0300

    KS-8-5 created enter ingredients function

--- src/main/kotlin/lesson_8/lesson_8_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_5.kt
@@ -1,5 +1,18 @@
 package org.example.lesson_8
 
 fun main() {
+    println("Enter the planned number of ingredients:")
+    val ingredientCount = readln().toInt()
+    val loopRange = 0 until ingredientCount
 
+    val recipe = Array(ingredientCount) {""}
+
+    for (i in loopRange) {
+        println("Enter the name of ${i + 1} ingredient:")
+        val ingredient = readln()
+
+        recipe[i] = ingredient
+    }
+
+    println("The recipe uses the following ingredients: ${recipe.joinToString(", ")}.")
 }
