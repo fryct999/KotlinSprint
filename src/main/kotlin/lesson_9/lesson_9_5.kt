9f0bae9955fee96af15342a110065c08c38ac581
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 14:14:40 +0300

    KS-9-5 created ingredient saver

--- src/main/kotlin/lesson_9/lesson_9_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_5.kt
@@ -1,5 +1,25 @@
 package org.example.lesson_9
 
 fun main() {
+    var ingredientCount = 1
+    val maxIngredient = 5
 
+    val ingredientList: MutableList<String> = mutableListOf()
+
+    while (ingredientCount <= maxIngredient) {
+        println("Enter the name of $ingredientCount ingredient:")
+        val ingredient = readln()
+
+        if (ingredientList.any { it.equals(ingredient, ignoreCase = true)}) {
+            println("Such an ingredient has already been added.")
+            continue
+        }
+
+        ingredientList.add(ingredient)
+        ingredientCount += 1
+    }
+
+    ingredientList.sort()
+    ingredientList[0] = ingredientList[0].replaceFirstChar { it.uppercase() }
+    println("You have added the following ingredients: ${ingredientList.joinToString(separator = ", ")}.")
 }
