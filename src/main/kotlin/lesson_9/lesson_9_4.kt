19f62453084047931277cc5c9323312aa238aa1d
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 13:48:34 +0300

    KS-9-4 created sort ingredient list

--- src/main/kotlin/lesson_9/lesson_9_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_4.kt
@@ -1,5 +1,13 @@
 package org.example.lesson_9
 
-fun main() {
+import kotlin.collections.sorted
 
+fun main() {
+    println("Enter the 5 ingredients using the separator - \", \" ")
+    val userIngredients = readln()
+
+    val ingredientList = userIngredients.split(", ")
+    val sortIngredientList = ingredientList.sorted()
+
+    println("You have entered the following ingredients: $sortIngredientList")
 }
