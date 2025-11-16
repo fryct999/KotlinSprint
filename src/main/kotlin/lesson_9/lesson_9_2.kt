5a0d247b7fc605d97dd600b2e6f65f4eef91aec2
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 12:58:14 +0300

    KS-9-2 created add ingredient function

--- src/main/kotlin/lesson_9/lesson_9_2.kt
+++ src/main/kotlin/lesson_9/lesson_9_2.kt
@@ -1,5 +1,18 @@
 package org.example.lesson_9
 
 fun main() {
+    val positiveAnswer = "Yes"
+    val negativeAnswer = "No"
+    val ingredientList = mutableListOf("Egg", "Salt", "Pepper")
 
+    println("The recipe has basic ingredients: $ingredientList")
+    println("Would you like to add another ingredient? ($positiveAnswer / $negativeAnswer)")
+
+    val userAnswer = readln()
+    if (userAnswer.equals(positiveAnswer, ignoreCase = true)) {
+        println("What ingredient do you want to add?")
+        ingredientList.add(readln())
+
+        println("Now the recipe has the following ingredients: $ingredientList")
+    }
 }
