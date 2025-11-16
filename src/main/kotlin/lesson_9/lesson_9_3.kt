9cd73c20794a70eff89d7753f595d6ea1fcaae85
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 13:28:24 +0300

    KS-9-3 created ingredients calc

--- src/main/kotlin/lesson_9/lesson_9_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_3.kt
@@ -1,5 +1,12 @@
 package org.example.lesson_9
 
 fun main() {
+    val omeletIngredientName = listOf("Egg", "Milk", "Butter")
+    val omeletIngredientCount = listOf(2, 50, 15)
 
+    println("Enter the number of portion:")
+    val portionNum = readln().toInt()
+
+    println("For $portionNum of portion you will need: ${omeletIngredientName[0]}: ${omeletIngredientCount[0] * portionNum}, " +
+            "${omeletIngredientName[1]}: ${omeletIngredientCount[1] * portionNum}, ${omeletIngredientName[2]}: ${omeletIngredientCount[2] * portionNum}")
 }
