454fe7126b4c6c6005758b27e77b3acc35d6e633
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 15:28:07 +0300

    KS-10-1 created dice game

--- src/main/kotlin/lesson_10/lesson_10_1.kt
+++ src/main/kotlin/lesson_10/lesson_10_1.kt
@@ -1,5 +1,19 @@
 package org.example.lesson_10
 
 fun main() {
+    val humanDiceNum = getDiceValue()
+    val botDiceNum = getDiceValue()
 
+    println("Human Dice Roll: $humanDiceNum")
+    println("Bot Dice Roll: $botDiceNum")
+
+    val msg = when {
+        humanDiceNum > botDiceNum -> "Humanity has won!"
+        humanDiceNum < botDiceNum -> "The computer won!"
+        else -> "Friendship has won!"
+    }
+
+    println(msg)
 }
+
+fun getDiceValue() : Int = (1..6).random()
