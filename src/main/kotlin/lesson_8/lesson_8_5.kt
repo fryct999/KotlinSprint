49583da29783b79a007f2faada0cc2c0822f5d20
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 19:30:24 +0300

    KS-8-5 code formating

--- src/main/kotlin/lesson_8/lesson_8_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_5.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Enter the planned number of ingredients:")
     val ingredientCount = readln().toInt()
 
-    val recipe = Array(ingredientCount) {""}
+    val recipe = Array(ingredientCount) { "" }
 
     for (i in recipe.indices) {
         println("Enter the name of ${i + 1} ingredient:")
