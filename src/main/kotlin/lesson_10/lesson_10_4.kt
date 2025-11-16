6a634765be36c747908e843cbe504502c1a8c6b3
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-16 16:18:53 +0300

    KS-10-4 created repeat dice game

--- src/main/kotlin/lesson_10/lesson_10_4.kt
+++ src/main/kotlin/lesson_10/lesson_10_4.kt
@@ -1,5 +1,43 @@
 package org.example.lesson_10
 
 fun main() {
+    var winsCounter = 0
+    var gameStart = true
+    val positiveAnswer = "Yes"
+    val negativeAnswer = "No"
 
+    while (gameStart) {
+        val gameResult = playGameRound()
+        if (gameResult == true) winsCounter += 1
+
+        println("Would you like to play it again? ($positiveAnswer / $negativeAnswer).")
+        if (readln().equals(negativeAnswer, ignoreCase = true)) gameStart = false
+    }
+
+    println("You have won $winsCounter games.")
 }
+
+fun playGameRound() : Boolean {
+    val humanDiceNum = getDiceValue()
+    val botDiceNum = getDiceValue()
+
+    println("Human Dice Roll: $humanDiceNum")
+    println("Bot Dice Roll: $botDiceNum")
+
+    when {
+        humanDiceNum > botDiceNum -> {
+            println("Humanity has won!")
+            return true
+        }
+        humanDiceNum < botDiceNum -> {
+            println("The computer won!")
+            return false
+        }
+        else -> {
+            println("Friendship has won!")
+            return false
+        }
+    }
+}
+
+fun getDiceValue() : Int = (1..6).random()
