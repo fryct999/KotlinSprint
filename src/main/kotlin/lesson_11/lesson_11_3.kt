8ab2dcf6a12066e98792ce5dc31e9ee4aa313405
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-19 02:35:44 +0300

    KS-11-3 created voice room and user classes

--- src/main/kotlin/lesson_11/lesson_11_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_3.kt
@@ -1,5 +1,36 @@
 package org.example.lesson_11
 
-fun main() {
+class User3(
+    val avatar: Any,
+    val name: String,
+    var status: String,
+) {}
 
+class VoiceRoom(
+    val avatar: Any,
+    val name: String,
+    val usersList: MutableList<User3>
+) {
+    val statusList = listOf("Speaking", "User muted", "Microphone muted")
+
+    fun addNewUser(user: User3) {
+        usersList.add(user)
+    }
+
+    fun changeUserStatus(userName: String, status: String): Boolean {
+        val user = usersList.find { it.name == userName }
+        if (user != null && statusList.contains(status)) {
+            user.status = status
+
+            return true
+        }
+
+        return false
+    }
+}
+
+fun main() {
+    val user1 = User3(404, "Lee", "Speaking")
+    val user2 = User3(404, "Lars", "Microphone muted")
+    val room1 = VoiceRoom(404, "Old camp", mutableListOf(user1, user2))
 }
