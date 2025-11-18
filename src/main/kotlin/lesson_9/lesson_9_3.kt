92aa2efda182fa6ddad78b77377905e26cef928b
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-18 22:40:11 +0300

    KS-9-3 change calc to list.map

--- src/main/kotlin/lesson_9/lesson_9_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_3.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_9
 
 fun main() {
-    val omeletIngredientName = listOf("Egg", "Milk", "Butter")
     val omeletIngredientCount = listOf(2, 50, 15)
 
     println("Enter the number of portion:")
     val portionNum = readln().toInt()
 
-    println("For $portionNum of portion you will need: ${omeletIngredientName[0]}: ${omeletIngredientCount[0] * portionNum}, " +
-            "${omeletIngredientName[1]}: ${omeletIngredientCount[1] * portionNum}, ${omeletIngredientName[2]}: ${omeletIngredientCount[2] * portionNum}")
+    val portionIngredientsCount = omeletIngredientCount.map { it * portionNum }
+    println("For $portionNum of portion you will need: Egg: ${portionIngredientsCount[0]}, Milk: ${portionIngredientsCount[1]}, Butter: ${portionIngredientsCount[2]}")
 }
