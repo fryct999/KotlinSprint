d81e4f9ddc5922c92b95ae4bf5ed664404bcdf60
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-19 02:52:33 +0300

    KS-11-4 created classes

--- src/main/kotlin/lesson_11/lesson_11_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_4.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_11
 
-fun main() {
+class Category(
+    val image: Any,
+    val name: String,
+    val descriptor: String,
+    val inFavorite: Boolean = false,
+)
 
-}
+class Recipe(
+    val image: Any,
+    val name: String,
+    val ingredient: Map<Ingredient, Int>,
+    val instruction: String,
+    val inFavorite: Boolean = false,
+)
+
+class Ingredient(
+    val name: String,
+)
