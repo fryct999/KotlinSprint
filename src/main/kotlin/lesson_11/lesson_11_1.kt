cc2debca424882b2cb65db91d00219e594f25952
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-19 01:25:26 +0300

    KS-11-1 created user class and get user data fun

--- src/main/kotlin/lesson_11/lesson_11_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_1.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_11
 
-fun main() {
+class User(
+    val id: Int,
+    val name: String,
+    val password: String,
+    val email: String,
+) {
+    fun getUserData(): String {
+        return "User name: $name, id: $id, password: $password, email: $email"
+    }
+}
 
+fun main() {
+    val user1 = User(1, "Biba", "1q2w3e4r5t", "bibamail@mail.ru")
+    val user2 = User(2, "Boba", "12345qwert", "bobamail@mail.ru")
+
+    println(user1.getUserData())
+    println(user2.getUserData())
 }
