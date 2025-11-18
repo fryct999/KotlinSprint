b276123722748126ff94079b3fdc067b9d18ecd0
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-19 01:59:07 +0300

    KS-11-2 created user class

--- src/main/kotlin/lesson_11/lesson_11_2.kt
+++ src/main/kotlin/lesson_11/lesson_11_2.kt
@@ -3,12 +3,37 @@ package org.example.lesson_11
 class User2(
     val id: Int,
     val name: String,
-    val password: String,
+    var password: String,
     val email: String,
+    var bio: String = "",
 ) {
+    fun changePassword() {
+        println("Enter old password:")
+        val oldPass = readln()
 
+        if (oldPass == password) {
+            println("Enter new password:")
+            password = readln()
+            println("Password successfully changed.")
+        }
+        else println("Password incorrect.")
+    }
+
+    fun addDataInBio() {
+        println("Add extra data:")
+        bio += readln()
+    }
+
+    fun getUserData() {
+        println("User id: $id, name: $name, password: $password, email: $email, bio: $bio")
+    }
 }
 
 fun main() {
+    val user = User2(1, "Nurgl", "1q2w3e4r5t", "chaosgarden@nurgl.com")
 
+    user.addDataInBio()
+    user.changePassword()
+
+    println(user.getUserData())
 }
