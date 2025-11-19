20516c6ef5514193d82bcd5c1baf9dd6e9ae8bc5
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-20 01:23:44 +0300

    KS-12-3 created weather class

--- src/main/kotlin/lesson_11_12/lesson_12_3.kt
+++ src/main/kotlin/lesson_11_12/lesson_12_3.kt
@@ -1,16 +1,22 @@
 package org.example.lesson_11_12
 
-class DailyMeteo3(
-    var nightTemperature: Int,
-    var dayTemperature: Int,
-    var haveRainfall: Boolean,
-) {
+const val KELVIN_COEFFICIENT = 273.15
+
+class DailyMeteo3() {
+    var nightTemperature: Int = 0
+    var dayTemperature: Int = 0
+    var haveRainfall = false
+
     fun printData() {
-        println(String.format("Today night temperature: $nightTemperature, day temperature: $dayTemperature, %s", if (haveRainfall)
-            "there will be precipitation today" else "no precipitation today"))
+        println(String.format("Today night temperature: ${nightTemperature - KELVIN_COEFFICIENT}, day temperature: ${dayTemperature - KELVIN_COEFFICIENT}, %s",
+            if (haveRainfall) "there will be precipitation today" else "no precipitation today"))
     }
 }
 
 fun main() {
+    val day1 = DailyMeteo3()
+    day1.nightTemperature = 230
+    day1.dayTemperature = 260
 
+    day1.printData()
 }
