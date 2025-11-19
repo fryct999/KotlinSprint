b8f8c5bb643b9f27dc0d6c94e8563e07030b2007
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-20 02:16:40 +0300

    KS-12-5 created branch

---
+++ src/main/kotlin/lesson_11_12/lesson_12_5.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_11_12
+
+const val KELVIN_COEFFICIENT = 273.15
+
+class DailyMeteo5() {
+    var nightTemperature: Int = 0
+    var dayTemperature: Int = 0
+    var haveRainfall = false
+
+    fun printData() {
+        println(String.format("Today night temperature: ${nightTemperature - KELVIN_COEFFICIENT}, day temperature: ${dayTemperature - KELVIN_COEFFICIENT}, %s",
+            if (haveRainfall) "there will be precipitation today." else "no precipitation today."))
+    }
+
+    init {
+        printData()
+    }
+}
+
+fun main() {
+    val day1 = DailyMeteo5()
+    day1.nightTemperature = 230
+    day1.dayTemperature = 260
+}
