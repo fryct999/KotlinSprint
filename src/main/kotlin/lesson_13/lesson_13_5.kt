513de88634848f6c56da350efb8d02f753f5a906
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-20 04:32:01 +0300

    KS-13-5 created branch

---
+++ src/main/kotlin/lesson_13/lesson_13_5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_13
+
+class Contact4(
+    val phoneNumber: Long,
+    val name: String,
+    val company: String? = null,
+) {
+    fun printContactData() {
+        println("Name: $name \n Phone number: $phoneNumber \n Company: ${company ?: "<Not data>"}")
+    }
+}
+
+fun main() {
+
+}
