e4b8eeb9b9006af5e9b852d92787d308f04bd579
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-20 03:43:58 +0300

    KS-13-3 print company

--- src/main/kotlin/lesson_13/lesson_13_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_3.kt
@@ -11,5 +11,15 @@ class Contact3(
 }
 
 fun main() {
+    val phoneBook = listOf<Contact3>(
+        Contact3(79999999999, "Tom", null),
+        Contact3(79999999998, "Jerry", null),
+        Contact3(79999999997, "Tinki", "null"),
+        Contact3(79999999996, "Vinki", "Burger shot"),
+        Contact3(79999999995, "Po", "IKEA"),
+    )
 
+    phoneBook.forEach {
+        if (it.company != null) println(it.company)
+    }
 }
