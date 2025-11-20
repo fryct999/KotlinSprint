6dcd36a22b3fd773d7f03e816de05c8e336e7ae9
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-20 04:29:40 +0300

    KS-13-4 created save to book function

--- src/main/kotlin/lesson_13/lesson_13_4.kt
+++ src/main/kotlin/lesson_13/lesson_13_4.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_13
 
-class Contact3(
+class Contact4(
     val phoneNumber: Long,
     val name: String,
     val company: String? = null,
@@ -11,5 +11,34 @@ class Contact3(
 }
 
 fun main() {
+    var addNumbers = true
+    val negativeAnswer = "no"
 
+    val phoneBook = mutableListOf<Contact4>()
+
+    while (addNumbers) {
+        println("Do you want to add a new contact? (yes/no)")
+        if (negativeAnswer == readLine()) {
+            addNumbers = false
+            break
+        }
+
+        println("Enter name:")
+        val name = readLine().toString()
+
+        println("Enter company:")
+        val company = readLine()
+
+        println("Enter phone number:")
+        val number = readLine()?.toLongOrNull()
+
+        if (number == null) {
+            println("The phone number was entered incorrectly.")
+            continue
+        }
+
+        phoneBook.add(Contact4(number, name, company))
+    }
+
+    phoneBook.forEach { it.printContactData() }
 }
