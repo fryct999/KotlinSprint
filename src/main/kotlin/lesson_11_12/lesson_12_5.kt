0ee05d659be1c9c77693dffee201799027b882c1
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-24 01:43:17 +0300

    KS-12-5 fixed average variables, fixed print string

--- src/main/kotlin/lesson_11_12/lesson_12_5.kt
+++ src/main/kotlin/lesson_11_12/lesson_12_5.kt
@@ -10,15 +10,17 @@ class DailyMeteo5(
 
 fun main() {
     val dayList = mutableListOf<DailyMeteo5>()
-    val temperatureRange = -10 .. 30
+    val temperatureRange = -10..30
 
     for (i in 1..30) {
         dayList.add(DailyMeteo5(temperatureRange.random(), temperatureRange.random(), Random.nextBoolean()))
     }
 
-    val dayWithRainFall = dayList.filter { it.hasRainfall == true }
-    val nightTemperatureList = "%.2f".format(dayList.map { it.dayTemperature }.average())
-    val dayTemperatureList = "%.2f".format(dayList.map { it.nightTemperature }.average())
+    val dayWithRainFall = dayList.filter { it.hasRainfall }
+    val dayTemperatureList = "%.2f".format(dayList.map { it.dayTemperature }.average())
+    val nightTemperatureList = "%.2f".format(dayList.map { it.nightTemperature }.average())
 
-    println("Average daytime temperature: $nightTemperatureList. Average temperature at night: $dayTemperatureList. Days with precipitation: ${dayWithRainFall.size}.")
+    println("Average daytime temperature: $nightTemperatureList. Average temperature at night: $dayTemperatureList. " +
+                "Days with precipitation: ${dayWithRainFall.size}."
+    )
 }
