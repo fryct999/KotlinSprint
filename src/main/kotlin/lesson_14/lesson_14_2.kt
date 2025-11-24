999e6b615118a68907b858ccb219d342479d9206
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-24 03:05:20 +0300

    KS-14-2 created ship data and loading function

--- src/main/kotlin/lesson_14/lesson_14_2.kt
+++ src/main/kotlin/lesson_14/lesson_14_2.kt
@@ -6,21 +6,49 @@ open class LinerShip(
     val cargoVolume: Int = 0,
     val passengerCount: Int = 0,
     val canIceBreaking: Boolean = false,
-) {}
+) {
+    fun printShipData() {
+        val iceString = if (canIceBreaking) "can break ice" else "can't break the ice"
+
+        println("Ship name: $name. Max ship speed: $speed. The ship holds $cargoVolume tons of cargo." +
+                "The ship accommodates $passengerCount passengers. The ship $iceString.")
+    }
+
+    open fun loading() {
+        println("$name extends the horizontal ramp.")
+    }
+}
 
 class IndustrialShip(
     name: String,
     speed: Int,
     cargoVolume: Int,
-) : LinerShip(name, speed, cargoVolume) {}
+) : LinerShip(name, speed, cargoVolume) {
+    override fun loading() {
+        println("$name activates the loading crane.")
+    }
+}
 
 class IcebreakerShip(
     name: String,
     speed: Int,
     cargoVolume: Int,
     canIceBreaking: Boolean = true,
-) : LinerShip(name, speed, cargoVolume, canIceBreaking = canIceBreaking) {}
+) : LinerShip(name, speed, cargoVolume, canIceBreaking = canIceBreaking) {
+    override fun loading() {
+        println("$name opens the gate from the back.")
+    }
+}
 
 fun main() {
+    val liner1 = LinerShip(name = "Liner1", speed = 500, cargoVolume = 150, passengerCount = 300)
+    val industrial1 = IndustrialShip(name = "Industrial1", speed = 320, cargoVolume = 500)
+    val icebreakerShip1 = IcebreakerShip(name = "iceBreaker1", speed = 300, cargoVolume = 100)
 
+    liner1.printShipData()
+    println()
+    industrial1.printShipData()
+    println()
+    icebreakerShip1.printShipData()
+    println()
 }
