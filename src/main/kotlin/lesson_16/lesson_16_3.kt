3344695828d36cbaaf1f408395bf69324c2cbfb1
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 01:35:26 +0300

    KS-16-3 created password validation

--- src/main/kotlin/lesson_16/lesson_16_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_3.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_16
 
-fun main() {
+class User(
+    val login: String,
+    private val password: String,
+) {
+    fun validation(password: String) : Boolean {
+        return this.password == password
+    }
+}
 
+fun main() {
+    val user1 = User(login = "Higart", password = "1q2w3e4r5t")
+
+    println("Enter password:")
+    val pass = readln().toString()
+
+    if (user1.validation(pass)) println("Password correct!")
+    else println("Password incorrect!")
 }
