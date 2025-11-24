7afc4140eb1dbec7e51f2490eda9d4e5d09f07b6
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-24 05:35:44 +0300

    KS-15-1 created fly and swim animals classes

--- src/main/kotlin/lesson_15/lesson_15_1.kt
+++ src/main/kotlin/lesson_15/lesson_15_1.kt
@@ -1,2 +1,50 @@
 package org.example.lesson_15
 
+interface FlyMovable {
+    fun startMove() {
+        println("Began to climb.")
+    }
+
+    fun endMove() {
+        println("Began to descend.")
+    }
+}
+
+interface SwimMovable {
+    fun startMove() {
+        println("Started diving.")
+    }
+
+    fun endMove() {
+        println("Rise to the surface.")
+    }
+}
+
+abstract class Animal {
+    abstract val name: String
+    abstract fun movement()
+}
+
+class FlyAnimal(override val name : String) : Animal(), FlyMovable {
+    override fun movement() {
+        startMove()
+        endMove()
+    }
+}
+
+class SwimAnimal(override val name : String) : Animal(), SwimMovable {
+    override fun movement() {
+        startMove()
+        endMove()
+    }
+}
+
+fun main() {
+    val caras = SwimAnimal("Caras")
+    val chaika = FlyAnimal("Chaika")
+    val duck = FlyAnimal("Duck")
+
+    caras.movement()
+    chaika.movement()
+    duck.movement()
+}
