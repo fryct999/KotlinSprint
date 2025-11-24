e601dd0d31201e37bf37df86b4b288ec47a1163d
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 02:23:49 +0300

    KS-16-5 created player class

--- src/main/kotlin/lesson_16/lesson_16_5.kt
+++ src/main/kotlin/lesson_16/lesson_16_5.kt
@@ -1,5 +1,41 @@
 package org.example.lesson_16
 
-fun main() {
+class Player(
+    val name: String,
+    var damage: Int,
+    val maxHP: Int,
+) {
+    private var hp = maxHP
+    private var canHeal = true
 
+    private fun death() {
+        canHeal = false
+        damage = 0
+        hp = 0
+
+        println("$name is dead.")
+    }
+
+    fun heal(heal: Int) {
+        if (!canHeal) return
+
+        hp += heal
+        if (hp > maxHP) hp = maxHP
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp <= 0) death()
+    }
+}
+
+fun main() {
+    val player1 = Player(name = "Dendi", damage = 10, maxHP = 150)
+
+    player1.takeDamage(10)
+    player1.takeDamage(20)
+    player1.takeDamage(20)
+    player1.heal(30)
+    player1.takeDamage(70)
+    player1.takeDamage(60)
 }
