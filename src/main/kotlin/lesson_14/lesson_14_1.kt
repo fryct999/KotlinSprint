01e86e82902ed767a2eec2791477a4e744bf2406
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 00:42:26 +0300

    KS-14-1 add override variables

--- src/main/kotlin/lesson_14/lesson_14_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_1.kt
@@ -1,25 +1,25 @@
 package org.example.lesson_14
 
 open class LinerShip(
-    val name: String,
-    val speed: Int,
-    val cargoVolume: Int = 0,
-    val passengerCount: Int = 0,
-    val canIceBreaking: Boolean = false,
-) {}
+    open val name: String,
+    open val speed: Int,
+    open val cargoVolume: Int = 0,
+    open val passengerCount: Int = 0,
+    open val canIceBreaking: Boolean = false,
+)
 
 class IndustrialShip(
     name: String,
     speed: Int,
-    cargoVolume: Int,
-) : LinerShip(name, speed, cargoVolume) {}
+    override val cargoVolume: Int,
+) : LinerShip(name, speed)
 
 class IcebreakerShip(
     name: String,
     speed: Int,
-    cargoVolume: Int,
-    canIceBreaking: Boolean = true,
-) : LinerShip(name, speed, cargoVolume, canIceBreaking = canIceBreaking) {}
+    override val  cargoVolume: Int,
+    override val  canIceBreaking: Boolean = true,
+) : LinerShip(name, speed)
 
 fun main() {
     val liner1 = LinerShip(name = "Liner1", speed = 500, cargoVolume = 150, passengerCount = 300)
