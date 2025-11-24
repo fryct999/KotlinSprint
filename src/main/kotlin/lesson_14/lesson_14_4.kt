c5e6d21d7657febc50f3dab660a4f5b56f1eaad0
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-24 03:51:57 +0300

    KS-14-4 created celestial body classes

--- src/main/kotlin/lesson_14/lesson_14_4.kt
+++ src/main/kotlin/lesson_14/lesson_14_4.kt
@@ -1,5 +1,32 @@
 package org.example.lesson_14
 
-fun main() {
+abstract class CelestialBody(
+    val name: String,
+    val hasAtmosphere: Boolean,
+    val hasLandingPossible: Boolean,
+)
 
+class Moon(
+    name: String,
+    hasAtmosphere: Boolean,
+    hasLandingPossible: Boolean,
+) : CelestialBody(name, hasAtmosphere, hasLandingPossible) {}
+
+class Planet(
+    name: String,
+    hasAtmosphere: Boolean,
+    hasLandingPossible: Boolean,
+    val moonList: List<Moon>,
+) : CelestialBody(name, hasAtmosphere, hasLandingPossible) {
+    fun printData() {
+        println("Planet $name has ${moonList.size} satellite: ${moonList.joinToString(separator = ", ") { it.name }}")
+    }
+}
+
+fun main() {
+    val moon1 = Moon(name = "moon1", hasAtmosphere = false, hasLandingPossible = true)
+    val moon2 = Moon(name = "moon2", hasAtmosphere = true, hasLandingPossible = false)
+    val planet1 = Planet(name = "planet1", hasAtmosphere = true, hasLandingPossible = false, moonList = listOf(moon1, moon2))
+
+    planet1.printData()
 }
