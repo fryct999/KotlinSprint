61414e28ddef1c24f6b8c99f31c1ace1db8feb5d
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 01:20:08 +0300

    KS-16-2 created circle class

--- src/main/kotlin/lesson_16/lesson_16_2.kt
+++ src/main/kotlin/lesson_16/lesson_16_2.kt
@@ -1,5 +1,24 @@
 package org.example.lesson_16
 
-fun main() {
+import kotlin.math.pow
 
+class Circle(
+    protected val radius: Double,
+) {
+    private val pi = 3.14
+
+    fun getArea() : Double {
+        return pi * radius.pow(2)
+    }
+
+    fun getLength() : Double {
+        return 2 * pi * radius
+    }
+}
+
+fun main() {
+    val circle1 = Circle(20.0)
+
+    println(circle1.getArea())
+    println(circle1.getLength())
 }
