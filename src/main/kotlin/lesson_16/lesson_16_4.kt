24ed38f7de5b2021eb769dc698b69ce28c90c52a
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 01:59:46 +0300

    KS-16-4 created order class

--- src/main/kotlin/lesson_16/lesson_16_4.kt
+++ src/main/kotlin/lesson_16/lesson_16_4.kt
@@ -1,5 +1,14 @@
 package org.example.lesson_16
 
-fun main() {
+class Order(
+    private val number: Int,
+    private var status: String,
+) {
+    fun changeStatus(newStatus: String) {
+        status = newStatus
+    }
+}
 
+fun callToManager(order: Order, status: String) {
+    order.changeStatus(status)
 }
