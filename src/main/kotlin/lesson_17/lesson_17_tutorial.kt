df3e9ad95fecfb1b8dacc3391b629c1b91183e6f
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-25 04:29:08 +0300

    KS-17 add tutorial

---
+++ src/main/kotlin/lesson_17/lesson_17_tutorial.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_17
+
+fun main() {
+    val fish1 = BabelFish(null)
+
+    println("old value: ${fish1.nerveSignalLevel}")
+//    println("old value: ${fish.isTranslated}")
+//    fish.nerveSignalLevel = 400
+//    println("new value: ${fish.nerveSignalLevel}")
+//    println("new value: ${fish.isTranslated}")
+
+    val fish2 = BabelFish(2)
+    println("old value: ${fish2.nerveSignalLevel}")
+
+    val fish3 = BabelFish(21)
+    println("old value: ${fish3.nerveSignalLevel}")
+}
