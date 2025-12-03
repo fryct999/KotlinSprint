7fd05bc2661ff407c2057443bc0936ecbe488940
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-03 05:23:00 +0300

    KS-18-4 created package class

--- src/main/kotlin/lesson_18/lesson_18_4.kt
+++ src/main/kotlin/lesson_18/lesson_18_4.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_18
 
-fun main() {
+class Package(
+    private val height: Int,
+    private val wide: Int,
+) {
+    constructor(size: Int) : this(size, size)
 
+    fun getArea(): Int {
+        return height * wide
+    }
+}
+
+fun main() {
+    val package1 = Package(10, 5)
+    val package2 = Package(6)
+
+    println(package1.getArea())
+    println(package2.getArea())
 }
