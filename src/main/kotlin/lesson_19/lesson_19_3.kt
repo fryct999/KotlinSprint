dbe13b4f7ff1b3e2b607b0680127a2bb44d2fe8f
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-10 02:37:34 +0300

    KS-19-3 created starship class

--- src/main/kotlin/lesson_19/lesson_19_3.kt
+++ src/main/kotlin/lesson_19/lesson_19_3.kt
@@ -1,5 +1,26 @@
 package org.example.lesson_19
 
-fun main() {
+class StarShip193(
+    val name: String,
+    val hp: Int,
+) {
+    fun shooting() {
+        TODO("Make a shooting method.")
+    }
 
+    fun takeOff() {
+        //TODO Add extra functions
+        println("Ship take off")
+    }
+
+    fun boarding() {
+        println("Ship boarding.")
+    }
+}
+
+fun main() {
+    val ship = StarShip193("Caracal", 150)
+    ship.boarding()
+    ship.takeOff()
+    ship.shooting()
 }
