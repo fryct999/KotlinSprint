d96715a5e54718a32f3f2348ac047a15f8764f51
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-10 17:34:12 +0300

    KS-22-2 tostring default and data classes

---
+++ src/main/kotlin/lesson_22/lesson_22_2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_22
+
+class RegularBook222(
+    val name: String,
+    val author: String,
+)
+
+data class DataBook222(
+    val name: String,
+    val author: String,
+)
+
+fun main() {
+    val book = RegularBook222("Nemesis games", "James S. A. Corey")
+    val dataBook = DataBook222("Nemesis games", "James S. A. Corey")
+
+    println(book) // будет выведено строковое представление объекта по умолчанию: название пакета.имя класса.хеш код
+    println(dataBook) // будет выведено осмысленное строковое представление класса: Название класса(все свойства объявленные в основном конструкторе)
+}
