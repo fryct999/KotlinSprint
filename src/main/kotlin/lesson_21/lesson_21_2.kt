2231f3a0627ba2d8e30e0affa19fda34b964ecf8
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-10 16:05:16 +0300

    KS-21-2 refactor extension fun

--- src/main/kotlin/lesson_21/lesson_21_2.kt
+++ src/main/kotlin/lesson_21/lesson_21_2.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_21
 
 fun List<Int>.evenNumbersSum(): Int {
-    var sum = 0
-
-    this.forEach {
-        if (it % 2 == 0) sum += it
-    }
+    val evenNums = filter { it % 2 == 0 }
+    val sum = evenNums.sum()
 
     return sum
 }
 
 fun main() {
-    val list = listOf(52, 23, 11, 12, 1, 6, 0, 33)
+    val list = listOf(52, 23, 12, 11, 1, 6, 0, 33)
     println(list.evenNumbersSum())
 }
